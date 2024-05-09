10c69c299e5144b57ef765006508179b63b22a2e
Jonathan van Rij <jonathan@blijnder.nl>
2024-05-09 11:34:41 +0200

    Vanilla installation of Laravel 11 and Nova 4 with User seeder

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory([
+            'name' => 'Jonathan van Rij',
+            'email' => 'jonathan@blijnder.nl',
+            'password' => Hash::make('secret'),
+        ])->create();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
